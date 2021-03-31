69d8fce4338afd071dc4584d5c357b44d957141f
henrichsen <harthenrichsen@gmail.com>
2021-03-31 11:32:40 -0600

    Initial Commit:
    Start entry and table react components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class tags_div extends React.Component{
+
 }
 
+class entry extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            expandedState: false,
+            color:props.position % 2=== 1 ? 'light-theme':'dark-theme', //may need to change props.color
+            metadata: props.metadata,
+        }
+        this.expandedState = this.expandedState.bind(this);
+        this.color = this.color.bind(this);
+        this.metadata = this.metadata.bind(this);
+
+    }
+
+    changeExpandedState(){ //onclick method
+        this.expandedState= !this.expandedState;
+    }
+
+
+
+    render() {
+        if(this.expandedState){
+            return(
+                //todo: add expanded return
+            );
+        }
+        else{
+            return (
+                //todo: add compressed return
+
+            );
+        }
+    }
+
+}
+
+class table extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state ={
+            entries:[],
+        }
+        this.entries = this.entries.bind(this);
+
+    }
+    render() {
+        const rows=[];
+        let counter = 1;
+        this.entries.forEach((currentEntry)=> {
+            if (true) { //todo: add if statement when add searching
+                rows.push(
+                    <entry position={counter} metadata={currentEntry}/>
+                )
+               counter += 1;
+            }
+        });
+        return(
+          <div class="app-table">
+              {rows}
+          </div>
+        );
+    }
+
+}
+
+
+
+
+
+}
 export default App;
